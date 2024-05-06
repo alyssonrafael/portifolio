ee8f45f89e5ad2ad307bc0521042cae6b20ef17a
Alysson Rafael <alyssonrafael11@gmail.com>
2024-05-06 00:15:43 -0300

    Card Technology Error Correction

--- src/components/Technologie.jsx
+++ src/components/Technologie.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Technologie = ({ text, Icon, iconClassName, className }) => {
   return (
-    <div className={` items-center bg-white dark:bg-gray-900 shadow-md rounded-lg p-2 hover:shadow-lg border border-transparent transition duration-500 ease-in-out transform hover:-translate-y-2 ${className}`}>
+    <div className={` flex items-center bg-white dark:bg-gray-900 shadow-md rounded-lg p-2 hover:shadow-lg border border-transparent transition duration-500 ease-in-out transform hover:-translate-y-2 ${className}`}>
       <Icon className={`text-4xl ${iconClassName}`} />
       <span className={`flex-1 text-center`}>{text}</span>
     </div>
