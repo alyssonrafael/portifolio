2eb40d8890687b3217505ea20c35f84646c4c205
Alysson Rafael <alyssonrafael11@gmail.com>
2024-05-07 11:22:54 -0300

    update navbar style

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -23,7 +23,7 @@ const Navbar2 = () => {
 
   return (
     <nav className="bg-transparent text-gray-800 dark:text-white text-1xl lg:text-2xl fixed top-0 w-full z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px6 lg:px-8 backdrop-filter backdrop-blur-sm">
+      <div className=" mx-auto px-4 sm:px6 lg:px-12 backdrop-filter backdrop-blur-sm">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0 mr-auto">
