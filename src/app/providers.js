88091ebf5f65805f87bfefca4834e688c48b4b1c
Alysson Rafael <alyssonrafael11@gmail.com>
2024-05-10 13:44:22 -0300

    theme change update

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,20 +1,18 @@
 "use client"
 
 import { ThemeProvider } from "next-themes";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function Providers({children}) {
+export default function Providers({ children }) {
+  const [mounted, setMounted] = useState(false);
 
-    const [mounted, setMounted] = useState(false)
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
+  if (!mounted) {
+    return <div style={{ visibility: 'hidden' }}>{children}</div>;
+  }
 
-    useEffect(() => {
-        setMounted(true)
-    },[])
-
-    if(!mounted) {
-        return <>{children}</>
-    }
-
-    return <ThemeProvider attribute="class">{children}</ThemeProvider>
+  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
 }
